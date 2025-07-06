2fa74930b8834f998e4ee6363f252ba8b141070c
亦世凡华 <97885806+ztK63LrD@users.noreply.github.com>
2025-07-06 17:31:21 +0800

    单元测试调整

--- packages/components/button/__tests__/index.test.tsx
+++ packages/components/button/__tests__/index.test.tsx
@@ -11,14 +11,19 @@ const testProps: ButtonProps = {
     size: 'large',
     className: 'ease-test',
 }
+const disabledProps: ButtonProps = {
+    disabled: true,
+    onClick: vi.fn()
+}
 
 describe("test EButton components", () => {
     it("应呈现正确的默认按钮", () => {
         const wrapper = render(<EButton {...defaultProps}>测试按钮</EButton>);
-        const element = wrapper.getByText("测试按钮"); // 获取文本为"测试按钮"的元素
+        const element = wrapper.getByText("测试按钮") as HTMLButtonElement; // 获取文本为"测试按钮"的元素
         expect(element).toBeInTheDocument(); // 断言该元素存在于文档中
         expect(element.tagName).toBe("BUTTON"); // 断言该元素的标签名为"BUTTON"
         expect(element).toHaveClass("ease-button"); // 断言该元素具有类名"ease-button"
+        expect(element.disabled).toBeFalsy(); // 断言该元素未被禁用
         fireEvent.click(element);
         expect(defaultProps.onClick).toHaveBeenCalled(); // 断言onClick函数被调用
     });
@@ -29,9 +34,17 @@ describe("test EButton components", () => {
         expect(element).toHaveClass("ease-button ease-button--primary ease-button--large ease-test");
     });
     it("当btnType等于link并且提供了href时，应呈现链接", () => {
-
-    })
-    it("should render disabled button when disabled set to true", () => {
-
+        const wrapper = render(<EButton href="https://www.baidu.com">测试按钮</EButton>);
+        const element = wrapper.getByText("测试按钮");
+        expect(element).toBeInTheDocument();
+        // expect(element.tagName).toBe("A");
+        // expect(element).toHaveClass("ease-button ease-button--link");
+    });
+    it("禁用时应呈现禁用按钮设置为true", () => {
+        const wrapper = render(<EButton {...disabledProps}>测试按钮</EButton>);
+        const element = wrapper.getByText("测试按钮") as HTMLButtonElement;
+        expect(element).toBeInTheDocument(); // 断言该元素存在于文档中
+        expect(element.disabled).toBeTruthy(); // 断言该元素已被禁用
+        expect(disabledProps.onClick).not.toHaveBeenCalled(); // 断言onClick函数未被调用
     })
 });
