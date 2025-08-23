17cf6a6fa431fbdf6a23577f1dc433b828742e95
亦世凡华 <97885806+ztK63LrD@users.noreply.github.com>
2025-08-23 23:10:14 +0800

    调整

--- docs/.vitepress/config/index.ts
+++ docs/.vitepress/config/index.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vitepress'
 import { groupIconVitePlugin } from 'vitepress-plugin-group-icons'
+import react from '@vitejs/plugin-react'
 import { shared } from './common/shared'
 import { zh } from './locales/zh'
 import { en } from './locales/en'
@@ -14,6 +15,16 @@ export default defineConfig({
 	vite: { // vite配置项，用于扩展vite插件等配置
 		plugins: [
 			groupIconVitePlugin() as any,
+			react({
+				jsxRuntime: 'automatic',
+				jsxImportSource: 'react'
+			})
 		],
+		optimizeDeps: {
+			include: ['react', 'react-dom']
+		},
+		ssr: {
+			noExternal: ['ease-reactify']
+		}
 	},
 })
--- docs/zh/component/button.md
+++ docs/zh/component/button.md
@@ -3,9 +3,6 @@ category: Components
 title: Button
 subtitle: 按钮
 description: 按钮用于开始一个即时操作。
-cover: https://mdn.alipayobjects.com/huamei_7uahnr/afts/img/A*7va7RKs3YzIAAAAAAAAAAAAADrJ8AQ/original
-coverDark: https://mdn.alipayobjects.com/huamei_7uahnr/afts/img/A*3T4cRqxH9-8AAAAAAAAAAAAADrJ8AQ/original
-designUrl: /docs/spec/buttons-cn
 demo:
   cols: 2
 group:
